8aa00a259795e26b3c258167b9647ce4c80f96bf
W3llz1n <wellintoh.silva.brito@gmail.com>
2022-04-27 19:28:29 -0300

    Play Store - 1.4.1

--- src/Pages/Jogos/index.js
+++ src/Pages/Jogos/index.js
@@ -1,10 +1,34 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { FlatList, StyleSheet, Text, View, ScrollView} from 'react-native';
+
+import Dados from "../../../dados/dadosJogo";
+import Cards from "../../components/card-telas/index";
 
 export default function Jogos() {
  return (
-   <View style={{backgroundColor:'#FDEB5C', flex:1}}>
-     <View></View>
-   </View>
+    <View style={{backgroundColor:'#FDEB5C', flex:1}}>
+       <FlatList
+       style={estilo.box_cards}
+        horizontal={false}
+        data = {Dados}
+        keyExtractor = {(item) => item.id}
+        renderItem = {({item}) => (
+
+          <Cards
+          titulo ={item.nome}
+          imagem ={item.imagem}
+          valor ={item.valor}
+          />
+        )}
+        />
+    </View>
   );
 }
+
+const estilo = StyleSheet.create({
+  box_cards:{
+    flexWrap:"wrap"
+  }
+
+
+});
---
+++ src/components/card-telas/index.js
@@ -0,0 +1,59 @@
+import React from "react";
+import {View,  TouchableOpacity, StyleSheet, Text, Image} from "react-native";
+
+export default function jogos({titulo, valor, imagem}){
+
+    function filterDesc(desc){
+        if(desc.length < 27){
+            return desc;
+        }
+        return `${desc.substring(0, 18)}...`;
+    }
+
+    return (
+        <>
+        <TouchableOpacity style={estilo.containerJogos}>
+            <Image
+                style={estilo.images}
+                source={require(`../../../assets/${imagem}`)}
+            />
+
+            <Text style={estilo.titulo}>{filterDesc(titulo)}</Text>
+            <Text style={estilo.valor}>{valor}</Text>
+
+        </TouchableOpacity>
+        </>
+    );
+}
+
+const estilo = StyleSheet.create({
+    containerJogos:{
+        backgroundColor: "#262223",
+        borderRadius: 10,
+        marginTop: 8,
+        padding: 10,
+        margin: 2,
+        alignItems: "center",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        // width: "100vw",
+        height: 100,
+    },
+    titulo:{
+      color: "#fff",
+      fontSize: 15,
+      fontWeight: 8,  
+    },
+    valor: {
+        color: "#D94C2A",
+        fontSize: 12,
+        marginLeft: 29,
+        marginTop: 2,
+        fontWeight: "bold",
+    },
+    images:{
+        width: "30%",
+        height: "100%",
+        borderRadius: 3,
+    },
+});
